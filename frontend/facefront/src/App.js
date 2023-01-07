432c72c11ff34a75e1d3e4c97f823c051dee6a29
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-01-07 19:55:07 +0530

    cards design done!!!

--- frontend/facefront/src/App.js
+++ frontend/facefront/src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Home from "./pages/Home";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 function App() {
+  useEffect(()=>{
+
+    AOS.init({
+      duration: 3000, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+    });
+  },[])
   return (
-    <h1>Routes!!</h1>
+    <>
+      <h1>Routes!!</h1>
+      <Home />
+    </>
   );
 }
 
