d114bed78eeba003360c6f6a9e5282617cb818e8
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-24 23:22:59 +0530

    New text style

--- frontend/facefront/src/components/Textanimation.jsx
+++ frontend/facefront/src/components/Textanimation.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
+import useInterval from 'react-use'
 export default function Textanimation() {
-  return <Wrapper>Ayush shukla</Wrapper>;
+  const textsplitarray = "RECOGNITION".split("");
+  const identification = "IDENTIFICATION".split("");
+  const [item,setitem]=useState(textsplitarray)
+  const [count,setcount]=useState(0)
+  const [play,setplay]=useState(false)
+
+  useInterval(()=>{
+    setitem(textsplitarray)
+    setcount(count+1)
+    if(count===0){
+        setcount(0)
+        setitem(identification)
+    }
+  }
+  ,play?6000:null)
+  useEffect(()=>{
+    const timer= setTimeout(() => {
+        setitem(identification)
+        setplay(true)
+    }, 4000);
+    return ()=>clearTimeout(timer)
+  },[])
+  return (
+    <Wrapper>
+      {textsplitarray.map((item, index) => {
+        <span key={index}>{item}</span>;
+      })}
+    </Wrapper>
+  );
 }
 
 const animation = keyframes`
@@ -35,5 +64,24 @@ const Wrapper = styled.span`
     animation-duration: 6s;
     animation-fill-mode: forwards;
     animation-iteration-count: infinite;
+    animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  span:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+  span:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  span:nth-child(3) {
+    animation-delay: 0.3s;
+  }
+  span:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+  span:nth-child(5) {
+    animation-delay: 0.5s;
+  }
+  span:nth-child(6) {
+    animation-delay: 0.6s;
   }
 `;
